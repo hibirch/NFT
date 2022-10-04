13e970b132e35fb5c511decef3d2478ebff6e565
hibirch <jiyou1726@163.com>
2022-10-04 15:49:54 +0800

    test

--- src/component/homePageHeader.tsx
+++ src/component/homePageHeader.tsx
@@ -1,5 +1,13 @@
 const HomePageHeader = () => {
-	return <div className=' container bg-red-300'>1</div>
+	return (
+		<div className='flex w-full items-center justify-between bg-red-100 py-8'>
+			<span className=' font-nova-flat text-2xl font-bold'>NFT</span>
+			<div className='flex items-center'>
+				<button>About</button>
+			</div>
+			<div>3</div>
+		</div>
+	)
 }
 
 export default HomePageHeader
